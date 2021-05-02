82de15940926d656681888b4bebc8cb67a70c869
Artem <arlomov@gmail.com>
2021-05-02 22:08:27 +0300

    Выполнил второе практическое задание(не смог сделать циклы и второе условие)

--- file.js
+++ file.js
@@ -1,3 +1,4 @@
+
 // Дз часть 1
 "use strict";
 
@@ -11,12 +12,28 @@ let personalMovieDB = {
   privat: false,
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', '');
-let b = prompt('На сколько оцените его?', '');
-let c = prompt('Один из последних просмотренных фильмов?', '');
-let d = prompt('На сколько оцените его?', '');
+if (personalMovieDB.count <=10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count <= 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Один из последних просмотренных фильмов?', '');
+  let b = prompt('На сколько оцените его?', '');
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
+
+
+
