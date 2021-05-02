672f40eb3fcc44bbfe86e9e05881aea86518113d
Artem <arlomov@gmail.com>
2021-05-02 19:33:51 +0300

    Выполнил первое дз

---
+++ file.js
@@ -0,0 +1,22 @@
+// Дз часть 1
+"use strict";
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a = prompt('Один из последних просмотренных фильмов?', '');
+let b = prompt('На сколько оцените его?', '');
+let c = prompt('Один из последних просмотренных фильмов?', '');
+let d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
