66bbf1f882db3230b496796f3d036340f0ec9839
Artem <arlomov@gmail.com>
2021-05-03 22:26:42 +0300

    Выполнил воторое паркт.занятие.Из двуз задач сделал только одну самостоятельно

--- file.js
+++ file.js
@@ -2,7 +2,17 @@
 // Дз часть 1
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -12,26 +22,44 @@ let personalMovieDB = {
   privat: false,
 };
 
-if (personalMovieDB.count <=10) {
-  alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count <= 30) {
-  alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-  alert('Вы киноман');
-} else {
-  alert('Произошла ошибка');
-}
-
-for (let i = 0; i < 2; i++) {
-  let a = prompt('Один из последних просмотренных фильмов?', '');
-  let b = prompt('На сколько оцените его?', '');
-
-  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-  } else {
-    i--;
+function showMyDB() {
+  if (personalMovieDB.privat == false) {
+    console.log(personalMovieDB);
   }
 }
+
+showMyDB();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count <=10) {
+    alert('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+  } else {
+    alert('Произошла ошибка');
+  }
+}
+
+detectPersonalLevel();
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    let b = prompt('На сколько оцените его?', '');
+  
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+console.log(personalMovieDB);
 
 /*
 // Тренировка с разными циклами 2 задание 
@@ -61,7 +89,7 @@ do {
 } while (i < 2);
 */
 
-console.log(personalMovieDB);
+
 
 
 
